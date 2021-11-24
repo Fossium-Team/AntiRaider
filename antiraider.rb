190717e3d81976dfd74e58a9d309abe6c8979120
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-11-24 13:01:56 +0100

    Add images

--- antiraider.rb
+++ antiraider.rb
@@ -1,1 +1,7 @@
+require 'discordrb'
 
+bot = Discordrb::Bot.new(token: 'token here', intents: [:server_messages, :server_members])
+
+bot.event.ServerMemberAddEvent do |event|
+  puts event.user.username
+end
