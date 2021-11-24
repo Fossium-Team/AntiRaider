0760647151c484e4928d9f10c9bdeaf85e5f0b1e
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-11-24 13:35:32 +0100

    oops

--- antiraider.rb
+++ antiraider.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new(token: 'OTEzMDMyNzM0MzIxMDM3MzEy.YZ4l_w.hIlnaJe_auiI-phI5tkHolCURzU', intents: [:servers, :server_messages, :server_bans, :server_emojis, :server_integrations, :server_webhooks, :server_invites, :server_voice_states, :server_presences, :server_message_reactions, :server_message_typing, :direct_messages, :direct_message_reactions, :direct_message_typing, :server_members])
+bot = Discordrb::Bot.new(token: 'token', intents: [:servers, :server_messages, :server_bans, :server_emojis, :server_integrations, :server_webhooks, :server_invites, :server_voice_states, :server_presences, :server_message_reactions, :server_message_typing, :direct_messages, :direct_message_reactions, :direct_message_typing, :server_members])
 
 bot.member_join do |event|
   puts event.user.username
