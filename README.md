a5aef3d4a63a45beaf2fc13243b2dc00b5fb3ed8
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-11-26 11:30:44 +0100

    Add json config

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ![AntiRaider](https://raw.githubusercontent.com/Fossium-Team/AntiRaider/main/images/AntiRaider_Full_White.png)
 # AntiRaider
 A simple anti-raid Discord Bot
+
+## Dependencies
+**Discordrb:**\
+Windows: `gem install discordrb --platform=ruby`\
+Linux/MacOS: `gem install discordrb`
--- antiraider.rb
+++ antiraider.rb
@@ -1,6 +1,10 @@
 require 'discordrb'
+require 'json'
 
-bot = Discordrb::Bot.new(token: 'token', intents: [:servers, :server_messages, :server_bans, :server_emojis, :server_integrations, :server_webhooks, :server_invites, :server_voice_states, :server_presences, :server_message_reactions, :server_message_typing, :direct_messages, :direct_message_reactions, :direct_message_typing, :server_members])
+jsonfile = File.read('./config.json')
+jsonhash = JSON.parse(jsonfile)
+
+bot = Discordrb::Bot.new(token: jsonhash['token'], intents: [:servers, :server_messages, :server_bans, :server_emojis, :server_integrations, :server_webhooks, :server_invites, :server_voice_states, :server_presences, :server_message_reactions, :server_message_typing, :direct_messages, :direct_message_reactions, :direct_message_typing, :server_members])
 
 bot.member_join do |event|
   puts event.user.username
